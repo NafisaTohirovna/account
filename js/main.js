359734d070683441bb4bfc79ec22c6fe45937a45
Nafisa <tohiirovna@gmail.com>
2023-04-21 22:07:42 +0500

    image upload add

--- js/main.js
+++ js/main.js
@@ -24,43 +24,16 @@ function setTheme(themeName) {
   //  darktheme end 
 
 
-//   function gender(num){
-//     if(num){
-//        document.getElementById('gen-girl').style.background = "#6137F8";
-//        document.getElementById('gen-boy').style.background = "#4F4C77";
-//     }else{
-//       document.getElementById('gen-boy').style.background = "#6137F8";
-//       document.getElementById('gen-girl').style.background = "#4F4C77";
-//     }
-//   }
 
 
-// const fileInput = document.getElementById("file");
-// const img = document.getElementById("img");
-
-// fileInput.addEventListener( 'change' , (e)=> {
-//   console.log(e);
+  const image_input = document.querySelector("#image_input");
+  let updownload_image = "";
   
-//   img.src = e.target.value;
-// }) 
-
-
-function readURL(input) {
-  if (input.files && input.files[0]) {
-    var reader = new FileReader();
-    reader.onload = function (e) {
-      $('#blah')
-        .attr('src', e.target.result);
-    };
-    reader.readAsDataURL(input.files[0]);
-  }
-}
-
-var node = document.getElementById('my-node');
-var btn = document.getElementById('dl');
-
-btn.onclick = function() {  domtoimage.toBlob(document.getElementById('my-node'))
-    .then(function(blob) {
-      window.saveAs(blob, 'nuc_creative.jpg');
-    });
-}
+  image_input.addEventListener("change" , function(){
+      const reader = new FileReader();
+      reader.addEventListener("load", () => {
+          updownload_image = reader.result;
+          document.querySelector("#disply_image").style.backgroundImage = `url(${updownload_image})`;
+      })
+      reader.readAsDataURL(this.files[0]);
+  })
